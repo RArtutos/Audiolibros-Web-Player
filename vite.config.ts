32f4504a9e6cb48abccb891ab20af492bac5c75a
Arturo Romero <116369556+RArtutos@users.noreply.github.com>
2024-11-10 11:24:50 -0600

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -22,19 +22,7 @@ export default defineConfig({
         target: 'http://localhost:8000',
         changeOrigin: true,
         secure: false,
-        ws: true,
-        rewrite: (path) => path,
-        configure: (proxy, _options) => {
-          proxy.on('error', (err, _req, _res) => {
-            console.log('proxy error', err);
-          });
-          proxy.on('proxyReq', (proxyReq, req, _res) => {
-            console.log('Sending Request:', req.method, req.url);
-          });
-          proxy.on('proxyRes', (proxyRes, req, _res) => {
-            console.log('Received Response:', proxyRes.statusCode, req.url);
-          });
-        }
+        rewrite: (path) => path
       }
     }
   }
